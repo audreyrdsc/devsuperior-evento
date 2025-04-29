b4565a9e8fbe99c9764103815292fc59c1030bf0
audreyrdsc <audreyrdsc@gmail.com>
2025-04-29 20:24:19 -0300

    Implementação dos relaciomentos com Categoria, Bloco e Participante

--- src/main/java/com/devsuperior/evento/entities/Atividade.java
+++ src/main/java/com/devsuperior/evento/entities/Atividade.java
@@ -6,6 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -21,5 +26,18 @@ public class Atividade {
     @Column(columnDefinition = "TEXT")
     private String descricao;
     private Double preco;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_atividade_participante",
+            joinColumns = @JoinColumn(name = "atividade_id"),
+            inverseJoinColumns = @JoinColumn(name = "participante_id"))
+    private Set<Participante> participantes = new HashSet<>();
 
 }
